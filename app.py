bf426cb067407f5068b86500443f33c4814177ec
HibranTapiaAtPernexium <157671037+HibranTapiaAtPernexium@users.noreply.github.com>
2025-12-11 17:48:20 -0600

    upd: added the error page

--- app.py
+++ app.py
@@ -375,6 +375,16 @@ def index():
         tabla_leads_columns= tabla_leads_columns,
     )
 
+@app.errorhandler(500)
+def handle_500(e):
+    return render_template("error_page.html"), 500
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        return e
+    return render_template("error_page.html"), 500
+
 ######################################### EJECUTADOR #############################################
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
